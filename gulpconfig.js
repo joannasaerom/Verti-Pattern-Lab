4ebd11f8869a5b4f011da689deea804ab49b34e7
Joanna Anderson <joanna.saerom.anderson@gmail.com>
2017-02-13 10:52:03 -0800

    Add logo atom, navigation molecule, and header organism

--- gulpconfig.js
+++ gulpconfig.js
@@ -47,7 +47,8 @@ module.exports = {
   js: {
     enabled: true,
     src: [
-      'js/**/*.js',
+      'js/script.js',
+      'js/script2.js',
       'source/_patterns/**/*.js',
     ],
     dest: 'dest/',
